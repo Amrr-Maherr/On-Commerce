8eb90a106cf3e4c9b5ba117125a8c8a470d3fc42
Amr Maher Ali <amrr.maherr24@gmail.com>
2026-09-19 17:03:35 +0300

    fix: update createProduct method to use CreateProductDto for type safety

--- src/products/controllers/products.controller.ts
+++ src/products/controllers/products.controller.ts
@@ -1,6 +1,7 @@
 import { Controller } from '@nestjs/common';
 import { Get, Post, Delete, Patch, Param, Query, InternalServerErrorException, Body } from '@nestjs/common';
 import { ProductsService } from '../services/products.service.js';
+import { CreateProductDto } from '../dto/create-product.dto.js';
 @Controller('products')
 export class ProductsController {
     constructor(private readonly productsService: ProductsService) { }
@@ -9,7 +10,7 @@ export class ProductsController {
 
     }
     @Post()
-    createProduct(@Body() newProduct: any) {
+    createProduct(@Body() newProduct: CreateProductDto) {
 
     }
     @Patch(":id")
