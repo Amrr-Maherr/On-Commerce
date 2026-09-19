a8fe70514841e678216b723af709b0c6966ffb56
Amr Maher Ali <amrr.maherr24@gmail.com>
2026-09-19 16:44:05 +0300

    feat: implement CRUD operations in ProductsController

--- src/products/controllers/products.controller.ts
+++ src/products/controllers/products.controller.ts
@@ -1,4 +1,27 @@
 import { Controller } from '@nestjs/common';
-
+import { Get, Post, Delete, Patch, Param, Query, InternalServerErrorException, Body } from '@nestjs/common';
+import { ProductsService } from '../services/products.service.js';
 @Controller('products')
-export class ProductsController { }
+export class ProductsController {
+    constructor(private readonly ProductsService: ProductsService) { }
+    @Get()
+    getProducts() {
+
+    }
+    @Post()
+    createProduct(@Body() newProduct: any) {
+
+    }
+    @Patch(":id")
+    updateProduct(@Param("id") id: string) {
+
+    }
+    @Delete(":id")
+    deleteProduct(@Param("id") id: string) {
+
+    }
+    @Get(":id")
+    singleProduct(@Param("id") id: string) {
+
+    }
+}
