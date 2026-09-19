829c116b902db4a4e4b7266e5d562c5d943f9848
Amr Maher Ali <amrr.maherr24@gmail.com>
2026-09-19 17:09:23 +0300

    fix: add validation messages for ProductId DTO properties

--- src/products/dto/product-id.dto.ts
+++ src/products/dto/product-id.dto.ts
@@ -1,6 +1,11 @@
-import { IsMongoId } from "class-validator";
+import { IsMongoId, IsNotEmpty } from "class-validator";
 
 export class ProductId {
-    @IsMongoId()
+    @IsNotEmpty({
+        message: 'Please provide a product ID',
+    })
+    @IsMongoId({
+        message: 'Please provide a valid product ID',
+    })
     id: string;
 }
